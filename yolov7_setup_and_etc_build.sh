0305ae5587c3857980a808630a0a9d43193d3362
chang558 <grab112@naver.com>
2025-04-09 12:29:22 +0900

    fix

--- yolov7_setup_and_etc_build.sh
+++ yolov7_setup_and_etc_build.sh
@@ -4,7 +4,7 @@ set -e
 USERNAME=${SUDO_USER:-$USER}
 
 #sudo chown -R $USER:$USER /home/terry/
-sudo find /home/terry -mindepth 1 -maxdepth 1 ! -name ".ssh" ! -name "thinclient_drives" -exec chown -R terry:terry {} \;
+sudo find /home/terry -mindepth 1 -maxdepth 1 -not -path "/home/terry/thinclient_drives" -exec chown -R terry:terry {} \; 2>/dev/null
 
 sudo chown -R terry:terry /home/terry/.ssh
 sudo chmod 700 /home/terry/.ssh
