3f52012b845d582158a5608d0c67636240642cd2
chang558 <grab112@naver.com>
2025-04-09 11:19:37 +0900

    'fix'

--- install_codium.sh
+++ install_codium.sh
@@ -1,6 +1,9 @@
+#!/bin/bash
+
 wget https://github.com/VSCodium/vscodium/releases/download/1.98.2.25078/codium_1.98.2.25078_arm64.deb
 
+sudo apt install -y ./codium_1.98.2.25078_arm64.deb
 
-sudo apt install ./codium_1.98.2.25078_arm64.deb
-
-codium
+echo ""
+echo "VSCodium 설치 완료. 실행하려면 'codium' 입력하세요."
+echo ""
